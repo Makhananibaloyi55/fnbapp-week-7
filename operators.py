0876957469e621c14d358a8b9ddbf19916787648
Makhananibaloyi55 <Makhananibaloyi55@gmail.com>
2025-06-21 09:44:07 +0200

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,14 @@
+#operators
+
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Division (/)
+# Modulus (%)
+# Exponent (**)
+
+x = 10
+x += 2
+
+print(x)
+
---
+++ strings.py
@@ -0,0 +1,6 @@
+#strings
+
+message = """kaylah's World
+is so cool"""
+
+print(message)
